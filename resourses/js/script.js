7bafb939f3b20c5745d9c810d2c44cf2f161392c
glous09 <goyavaes@hotmail.es>
2018-06-23 21:12:22 -0500

    barra pegajosa

---
+++ resourses/js/script.js
@@ -0,0 +1,13 @@
+//Menu pegajoso waypoint plugin
+
+$(document).ready(function(){
+    
+    $('.js--section-features').waypoint(function(direction) {
+        if(direction == "down"){
+            $('nav').addClass('sticky');
+            
+        }else{
+            $('nav').removeClass('sticky')
+        } 
+    }) 
+});

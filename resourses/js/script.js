ecd5074c4942960075665406db253a4361c5eeef
glous09 <goyavaes@hotmail.es>
2018-06-23 22:28:51 -0500

    Fin de barra pegajosa e iicio de scrolls

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,13 +1,30 @@
 //Menu pegajoso waypoint plugin
 
-$(document).ready(function(){
+$(document).ready(function() {
     
     $('.js--section-features').waypoint(function(direction) {
         if(direction == "down"){
             $('nav').addClass('sticky');
             
         }else{
-            $('nav').removeClass('sticky')
+            $('nav').removeClass('sticky');
         } 
-    }) 
+    }, {
+        offset: '60px;'
+    });
+    
+    //Scroll on buttons
+    
+    $('.js--scroll-to-plans').click(function() {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+        
+    });
+    
+    $('.js--scroll-to-start').click(function() {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+        
+    });
+    
+    
+   
 });
